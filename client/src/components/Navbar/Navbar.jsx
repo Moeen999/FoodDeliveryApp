29a4164cfcf083a4ae1d841f87e9cec92575b3cd
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-24 21:45:53 +0500

    Added the active state for the navbar list items and also added the psudo classes for the signin button and navbar list items for a better user interface and interaction

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contcat-us</li>
+        <li
+          onClick={() => setIsActive("home")}
+          className={isActive === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setIsActive("menu")}
+          className={isActive === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setIsActive("mobile-app")}
+          className={isActive === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setIsActive("contact-us")}
+          className={isActive === "contact-us" ? "active" : ""}
+        >
+          contact-us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="searchicon" />
