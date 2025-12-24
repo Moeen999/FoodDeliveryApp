d877131f36d1d0708f4fe3e2f5aaeb5465e14429
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-24 23:26:27 +0500

    Routes setup in main file and added the initial structure of the app such as components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import Home from "./components/pages/Home/Home"
+import Cart from "./components/pages/Cart/Cart"
+import PlaceOrder from "./components/pages/PlaceOrder/PlaceOrder"
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../header/header"
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
