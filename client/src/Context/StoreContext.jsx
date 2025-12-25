314cca365eea271edc219ac4e76403f32ec0e74a
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-25 18:05:36 +0500

    Added the cart state functionality into the context store also added the category wise food selection and a food counter

--- client/src/Context/StoreContext.jsx
+++ client/src/Context/StoreContext.jsx
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
+
 const ContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const addToCart = (itemID) => {
+    if (!cartItems[itemID]) {
+      setCartItems((prev) => ({ ...prev, [itemID]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] + 1 }));
+    }
+  };
+  const removeFromCart = (itemID) => {
+    setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }));
+  };
+  useEffect(()=>{
+    console.log(cartItems);
+  },[cartItems])
   const contextValues = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
   return (
     <StoreContext.Provider value={contextValues}>
--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,16 +10,18 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, idx) => {
-          return (
-            <FoodItem
-              key={idx}
-              id={item._id}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-              image={item.image}
-            />
-          );
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={idx}
+                id={item._id}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                image={item.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
