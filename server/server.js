a4e2b71c5fcbf37c08d365188f977f42f1886124
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-27 18:15:21 +0500

    Fixed the responsiveness issue in client and added the basic server in BE

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("Hello Nigga")
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 4000;
+app.get("/", (req, res) => {
+  res.send("Hello Ghareeb Nawaz");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at localhost://${PORT}`);
+});
