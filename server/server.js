024f00cd52795f8ac0f42066fb173581612ee3f0
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-28 00:06:58 +0500

    Implemented the logic for the add food controller in backend also added the routes and multer package for easy routing and image upload respectively

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 const app = express();
-
 
 app.use(express.json());
 app.use(cors());
@@ -14,8 +14,14 @@ app.get("/", (req, res) => {
   res.send("Hello Ghareeb Nawaz");
 });
 
-// DB Connection 
-  connectDB()
+// DB Connection
+connectDB();
+
+//   API endpoints
+
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
 app.listen(PORT, () => {
   console.log(`Server is running at localhost://${PORT}`);
 });
