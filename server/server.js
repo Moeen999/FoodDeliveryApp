1c867fd7805363660e3c47b5284e64ca9c349803
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-27 18:58:46 +0500

    Added the mongoDB atlas setup for DB and the models of the app

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Hello Ghareeb Nawaz");
 });
 
+// DB Connection 
+  connectDB()
 app.listen(PORT, () => {
   console.log(`Server is running at localhost://${PORT}`);
 });
