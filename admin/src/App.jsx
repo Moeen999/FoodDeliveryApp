96ac846f5f108b7f683d368ea609bc840bc809c1
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-28 15:03:20 +0500

    Added the sidebar and navbar component along with its styling and responsive design

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,16 @@
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
-      Admin Panel
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
