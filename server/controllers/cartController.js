3c4d7b01db3d1529a9cc54a1fb6a9b1e6193885c
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-30 12:45:49 +0500

    Implemented the logic to remove the item from cart in the BE controller function

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -18,7 +18,21 @@ const addToCart = async (req, res) => {
   }
 };
 
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    const { userId, itemId } = req.body;
+    let userData = await userModel.findOne({ _id: userId });
+    let cartData = await userData.cartData;
+    if (cartData[itemId] > 0) {
+      cartData[itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Item removed from Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error Occured" });
+  }
+};
 
 const getCartData = async (req, res) => {};
 
